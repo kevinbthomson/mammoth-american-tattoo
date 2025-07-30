dfad2f42770a59c6abbafebcbd19099951620e1d
Kevin Thomson <kevinbthomson@gmail.com>
2025-07-30 10:31:25 -0600

    added server component adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import sanity from '@sanity/astro';
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [
     sanity({
       projectId: 'i8y9tpei',

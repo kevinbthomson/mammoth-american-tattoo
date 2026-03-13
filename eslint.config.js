dfbfc23bd8b723e138b720d8101269c15452ecea
Kevin Thomson <kevinbthomson@gmail.com>
2026-03-13 16:14:27 -0600

    add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  // Ignore build output
+  {
+    ignores: ['dist/**', 'node_modules/**', '.astro/**'],
+  },
+
+  // TypeScript files
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+
+  // Astro files
+  ...eslintPluginAstro.configs.recommended,
+];
